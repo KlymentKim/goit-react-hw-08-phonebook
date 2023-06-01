91aaae5916d3fd12200c71c1f7ce5e03edebdb8f
Klyment <klim86kr@gmail.com>
2023-06-01 03:13:25 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,10 @@ export class App extends React.Component {
   
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Eduard Fury', number: '098-354-05-00' },
+      { id: 'id-2', name: 'Maksim Bekovikon', number: '063-458-00-77' },
+      { id: 'id-3', name: 'Marta Sifurukova', number: '050-489-66-44' },
+      { id: 'id-4', name: 'Anna Kuhta', number: '0564-4558-52' },
     ],
     filter: '',
   };
