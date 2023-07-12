97839267be2d6a26cba4628d3de3cf7f58183c91
Klyment <klim86kr@gmail.com>
2023-07-12 10:59:29 +0300

    add

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,14 +3,15 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 // Встановлення базового URL для axios
 axios.defaults.baseURL = 'https://64ad3c27b470006a5ec594cd.mockapi.io';
-
-// Створення асинхронної Thunk-дії fetchContacts
+// axios.defaults.baseURL = 'https://64a199d00079ce56e2db3eeb.mockapi.io';
+const path = '/contacts'
+// Thunk-дії fetchContacts - одержання масиву контактів (метод GET) запитом.
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll', // Унікальний рядок, що ідентифікує цю дію
   async (_, thunkAPI) => {
     try {
       // Надсилання GET-запиту на '/contacts'
-      const response = await axios.get('/contacts');
+      const response = await axios.get(path);
       // Повернення отриманих даних
       return response.data;
     } catch (error) {
@@ -23,10 +24,10 @@ export const fetchContacts = createAsyncThunk(
 // Створення асинхронної Thunk-дії addContacts
 export const addContacts = createAsyncThunk(
   'contacts/addContacts', // Унікальний рядок, що ідентифікує цю дію
-  async (contacts , thunkAPI) => {
+  async (contacts, thunkAPI) => {
     try {
       // Надсилання POST-запиту на '/contacts' з даними { name, number }
-      const response = await axios.post('/contacts', contacts);
+      const response = await axios.post(path, contacts);
       // Повернення отриманих даних
       return response.data;
     } catch (error) {
@@ -39,10 +40,10 @@ export const addContacts = createAsyncThunk(
 // Создание асинхронного Thunk-действия deleteContacts
 export const deleteContacts = createAsyncThunk(
   'contacts/deleteContacts', // Унікальний рядок, що ідентифікує цю дію
-  async (contactId, thunkAPI) => {
+  async (id, thunkAPI) => {
     try {
       // Надсилання DELETE-запиту на `/contacts/${contactId}`
-      const response = await axios.delete(`/contacts/${contactId}`);
+      const response = await axios.delete(`/${path}/${id}`);
       // Повернення отриманих даних
       return response.data;
     } catch (error) {
