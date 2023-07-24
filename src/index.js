89febc02e6ddeab77010b940c9d4cb11880a8323
Klyment <klim86kr@gmail.com>
2023-07-24 23:09:26 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
